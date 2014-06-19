f0615cfa09b27bbf6417685b98ad0126a5f3e43a
Luigi Bertaco <lucristofolini@gmail.com>
2014-06-19 00:56:03 +0000

    Adicionado menu Horas à Pagar com grid estático

---
+++ ponto/ponto/app/controllers/pagar_controller.rb
@@ -0,0 +1,9 @@
+class PagarController < ApplicationController
+  before_action :set_funcionario, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    
+  end
+  
+end
+  
--- ponto/ponto/config/routes.rb
+++ ponto/ponto/config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   resources :divergencias
   
   resources :inconsistencias
+  
+  resources :pagar
 
   root 'home#index'
   get 'importabatidas' => 'home#importabatidas'
