20ef1e17b006befe4cd6aafd48f1aceb958179de
Luigi Bertaco <lucristofolini@gmail.com>
2014-06-24 00:14:53 +0000

    Corrigido filtro do grid de funcionarios

--- ponto/ponto/app/assets/javascripts/application.js
+++ ponto/ponto/app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require bootstrap
 //= require bootstrap-datetimepicker
 //= require bootstrap-datetimepicker.pt-BR
 //= require zbootstrap
